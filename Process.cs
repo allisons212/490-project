70d33adcf48b80ab410ae22aa18503da60aaeec2
Sierra Elisa Laney <sierralaney@gmail.com>
2021-09-29 20:35:17 -0500

    Parser, ThreadSim, Process functions. ThreadSim uncompleted.

---
+++ Process.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Process
+{
+	public int arrivalTime;
+	public int serviceTime;
+	public string processID;
+	public int priority;
+	public DateTime entryTime;
+	public DateTime exitTime;
+	public Process()
+	{
+		arrivalTime = 0;
+		serviceTime = 0;
+		processID = "";
+		priority = 0;
+	}
+}
