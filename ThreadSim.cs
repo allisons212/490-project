ec749f6e2983ba4ac5a4459d73d7cbccc8f951d0
Sierra Elisa Laney <sierralaney@gmail.com>
2021-10-06 19:08:11 -0500

    Throughput and time interval and report array added.

--- ThreadSim.cs
+++ ThreadSim.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Threading;
+// using System.Threading.Tasks;
 using System.Collections.Generic;
+
 
 public class ThreadSim
 {
 	int tpNumber;
+
 	public ThreadSim()
 	{
-
+		Process[] reportArray = new Process[] { }
 	}
 
-	public static void executeProcess(Queue<Process> processQueue)
+	public static void executeProcess(Queue<Process> processQueue, int miliseconds)
 	{
+		int counter = 0; 
 		foreach (Process process in processQueue)
 		{
 			Console.WriteLine(process.processID + " is beginning execution.");
@@ -19,15 +23,25 @@ public class ThreadSim
 			for (int i = 0; i < process.serviceTime; i++)
 			{
 				Console.WriteLine(process.processID + " is executing.");
+				Wait(miliseconds);
 			}
 			process.exitTime = DateTime.Now;
+			reportArray[counter] = process;
+			counter++; 
 		}
 
 	}
 
-	public int computeThroughput(int time1, int time2)
+	public int computeThroughput(int time1, int time2, Process[] reportArray)
 	{
-
-		return 0;
+		int counter = 0;
+		foreach (Process process in reportArray)
+        {
+			if (process.exitTime > time1 && process.exitTime < time2)
+            {
+				counter++; 
+            }
+        }
+		return counter;
 	}
 }
